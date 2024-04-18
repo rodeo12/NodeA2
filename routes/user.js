a14c0228048f102594c5173d4eca5cd86778bfbb
Mayanke <mayankthakur1294@gmail.com>
2024-04-19 02:18:40 +0530

    added the authcontroller in routes file

--- routes/user.js
+++ routes/user.js
@@ -2,11 +2,12 @@
 const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/usersController');
+const authMiddleware = require('../middleware/authMiddleware');
 
 // Routes
 router.post('/', usersController.createUser); //Create User
-router.get('/', usersController.getUsers); //Get All Users
-router.get('/:id', usersController.getUserById); //Get User By Id
-router.get('/analytics', usersController.getUserAnalytics); // Analytics endpoint
+router.get('/', authMiddleware,usersController.getUsers); //Get All Users
+router.get('/:id',authMiddleware, usersController.getUserById); //Get User By Id
+router.get('/analytics',authMiddleware, usersController.getUserAnalytics); // Analytics endpoint
 
 module.exports = router;
